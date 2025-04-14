bd84048428a35c840ca3a5add02a169a5abb04ac
Aidam <55688345+Aidam7@users.noreply.github.com>
2025-04-14 15:31:44 +0200

    5&6

--- src/main/java/pro1/Main6.java
+++ src/main/java/pro1/Main6.java
@@ -1,19 +1,28 @@
 package pro1;
 
+import com.google.gson.Gson;
 import pro1.apiDataModel.ActionsList;
+
+import java.util.Comparator;
+import java.util.HashMap;
 
 public class Main6 {
 
     public static void main(String[] args) {
-        System.out.println(idOfBestTeacher("KIKM",2024));
+        System.out.println(idOfBestTeacher("KIKM", 2024));
     }
 
-    public static long idOfBestTeacher(String department, int year)
-    {
+    public static long idOfBestTeacher(String department, int year) {
         // TODO 6.1 (navazuje na TODO 3):
         //  - Stáhni seznam akcí na katedře (jiná data nepoužívat)
         //  - Najdi učitele s nejvyšším "score" a vrať jeho ID
-
-        return 0;
+        String json = Api.getActionsByDepartment(department, year);
+        ActionsList actions = new Gson().fromJson(json, ActionsList.class);
+        HashMap<Long, Integer> hashMap = new HashMap<>();
+        actions.items.stream().forEach(a -> {
+            int current = hashMap.getOrDefault(a.teacherId, 0);
+            hashMap.put(a.teacherId, current + a.personsCount);
+        });
+        return hashMap.entrySet().stream().max(Comparator.comparing(h -> h.getValue())).get().getKey();
     }
 }
--- src/test/java/pro1/Main6Test.java
+++ src/test/java/pro1/Main6Test.java
@@ -4,15 +4,13 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class Main6Test
-{
+public class Main6Test {
     @Test
-    void test01()
-    {
+    void test01() {
         // TODO 6.2: Oprav test
         assertEquals(
-                123,
-                Main6.idOfBestTeacher("KIKM",2024)
+                244417,
+                Main6.idOfBestTeacher("KIKM", 2024)
         );
     }
 }
